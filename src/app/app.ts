e66751a00ac1d86dfd281ee6fc151c8e6f9edbcc
Mayank <agarwalm92@gmail.com>
2025-08-23 21:16:06 +0530

    Initial Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './components/core/home/home';
+import { Aboutus } from './components/core/aboutus/aboutus';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Aboutus],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true, // new to angular 16+, if yes no ngmodule is used
 })
 export class App {
   protected readonly title = signal('OnlineClasses_UI');
