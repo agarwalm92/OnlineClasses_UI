33f0d571e55b7805794efe899ccfe62ea5aaca50
Mayank <agarwalm92@gmail.com>
2025-08-30 23:35:49 +0530

    routes in navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/core/home/home';
+import { Aboutus } from './components/core/aboutus/aboutus';
+import { Category } from './components/category/category';
+import { Pricingplan } from './components/pricingplan/pricingplan';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'aboutus', component: Aboutus },
+  { path: 'course/category', component: Category },
+  { path: 'pricingplan', component: Pricingplan },
+];
